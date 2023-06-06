b978a041256b41d24a616edce294ca1a37de22cb
roshan pujara <pujararoshan984@gmail.com>
2023-06-06 18:03:17 +0545

    feat:adding comments

--- slice/MovieSlice.tsx
+++ slice/MovieSlice.tsx
@@ -6,9 +6,13 @@ const initialState:MovieInterface = {
 const movieslice = createSlice({
     name:'Movies',
     initialState,
+    // (state,data): (state) represents the current state of redux store, it is the state that existed before the action is dispatched. (data) parameter represents the action object that was dispatched. In this case it contains, 'payload' property that holds the data passed to the action.
+    // This line updates the movies property of the state object with the value of data.payload.
     reducers:{
         setGlobalMovies:(state,data)=>{
             state.movies = data.payload;
+            // This line updates the movies property of the state object with the value of data.payload. (response.data.results) is passed as the payload.
+            
         }
     }
 
